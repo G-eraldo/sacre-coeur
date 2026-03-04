7298a6837a18efa21ce0f0420c19b7b753d86622
geraldo <contact@lafabriqueducode.fr>
2026-03-04 11:58:33 +0100

    ajout mentions légale

---
+++ app/pages/mentions-legales.vue
@@ -0,0 +1,95 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="min-h-screen">
+    <section class="pt-40 lg:pt-56 pb-20 bg-brand-warm relative overflow-hidden text-center">
+
+      <div class="pt-24 lg:pt-32 pb-16">
+
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+
+          <div class="max-w-3xl mx-auto text-center">
+            <h1 class="text-4xl font-bold tracking-tight text-brand-primary  sm:text-5xl">
+              MENTIONS LÉGALES
+            </h1>
+            <p class="mt-4 text-lg text-slate-600">
+              Informations légales et obligatoires concernant le site
+              <span class="text-brand-gold font-bold">demo-sacre-coeur.vercel.app</span>
+            </p>
+          </div>
+
+
+          <div class="mt-12 max-w-3xl mx-auto">
+
+            <p class="text-slate-600 mb-6 text-lg">
+              Conformément aux dispositions de la loi n° 2004-575 du 21 juin
+              2004 pour la confiance en l'économie numérique, il est précisé
+              aux utilisateurs du site <span class="text-brand-gold font-bold">sacre-coeur.fr</span> l'identité des
+              différents intervenants dans le cadre de sa réalisation et de
+              son suivi.
+            </p>
+
+
+            <h2 class="text-2xl font-bold mt-10 mb-4 text-brand-primary">
+              Edition du site
+            </h2>
+            <p class="text-slate-600 mb-4">
+              Le présent site, accessible à l’URL
+              <span class="text-brand-gold font-bold">
+                https://demo-sacre-coeur.vercel.app/
+              </span>
+              (le « Site »), est édité par :
+            </p>
+
+            <p class="text-slate-600 mb-4 pl-4 border-l-2 border-brand-gold">
+              Gérald Francois, résidant 27A grande rue 80250 Thory, de
+              nationalité Française (France), né(e) le 30/11/1994, inscrite au
+              R.C.S. de AMIENS sous le numéro 994 074 458 R.C.S. Amiens,
+            </p>
+
+
+            <h2 class="text-2xl font-bold text-brand-primary mt-10 mb-4">
+              Hébergement
+            </h2>
+            <p class="text-slate-600 mb-4">
+              Le Site est hébergé par la société Vercel Inc., situé 340 S
+              Lemon Ave #4133, Walnut, CA 91789, États-Unis, (contact
+              téléphonique ou email : +15592887060).
+            </p>
+
+
+            <h2 class="text-2xl font-bold text-brand-primary mt-10 mb-4">
+              Directeur de publication
+            </h2>
+            <p class="text-slate-600 mb-4">
+              Le Directeur de la publication du Site est Gérald Francois.
+            </p>
+
+
+            <h2 class="text-2xl font-bold text-brand-primary mt-10 mb-4">
+              Nous contacter
+            </h2>
+            <ul class="list-none space-y-2 text-slate-600">
+              <li>
+                <strong class="text-brand-gold">Par téléphone :</strong>
+                +33767265366
+              </li>
+              <li>
+                <strong class="text-brand-gold">Par email :</strong>
+                contact@lafabriqueducode.fr
+              </li>
+              <li>
+                <strong class="text-brand-gold">Par courrier :</strong> 27A
+                grande rue 80250 Thory
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<style scoped></style>
